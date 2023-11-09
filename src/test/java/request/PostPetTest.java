a21974b95fe9319b14079b021d80b1d9a8cecf58
Özgür Çubuk <ozgur.cubuk@netsparker.com>
2023-11-09 03:31:51 +0300

    minor mistake fixed again

--- src/test/java/request/PostPetTest.java
+++ src/test/java/request/PostPetTest.java
@@ -13,7 +13,7 @@ import static objects.PetStatus.available;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertNotNull;
 
-public class PostPet extends RequestBase {
+public class PostPetTest extends RequestBase {
 
     private final String path = "/pet";
 
